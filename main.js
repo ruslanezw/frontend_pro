67ec86b00d4fc216c330f6b33f89a0661128f6a4
ruslanezw <ruslanezw@gmail.com>
2024-09-05 16:19:05 +0300

    L3-HW2 completed

---
+++ main.js
@@ -0,0 +1,5 @@
+let firstString = prompt("What is your name?");
+let secondString = prompt("Where are you from?");
+let thirdString = prompt("What programming language are you learning?");
+
+console.log(`${firstString}, ${secondString}, ${thirdString}`);
