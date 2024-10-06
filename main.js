512b3024b81354514a61cf9a3a74fec52676cbd9
ruslanezw <ruslanezw@gmail.com>
2024-10-06 21:34:29 +0300

    L11-HW1 completed fixid

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-const table = document.querySelector(`#table`)
-
 function createPythagorasTable() {
-    const tablePythagor = document.createElement(`table`);
+    const table = document.querySelector(`#table`);
 
     for (let i = 1; i <= 10; i++) {
-        const tableRow = document.createElement(`tr`)
+        const tableRow = document.createElement(`tr`);
+        table.appendChild(tableRow);
 
         for (let j = 1; j <= 10; j++) {
             const tableCell = document.createElement(j === 1 || i === 1 ? 'th' : 'td');
             tableCell.textContent = i * j;
             tableRow.appendChild(tableCell);
         }
-
-        tablePythagor.appendChild(tableRow);
     }
-
-    table.appendChild(tablePythagor);
 }
 
 createPythagorasTable();
